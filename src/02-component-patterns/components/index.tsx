b177a27a8ace631b43c7bae43e36c6de38b6e452
emaelgani <emanuelelgani@gmail.com>
2025-01-26 18:57:21 -0300

    props component compound pattern

--- src/02-component-patterns/components/index.tsx
+++ src/02-component-patterns/components/index.tsx
@@ -2,6 +2,7 @@ import { ProductCard as ProductCardHOC} from "./ProductCard";
 import { ProductButtons } from "./ProductButtons";
 import { ProductImage } from "./ProductImage";
 import { ProductTitle } from "./ProductTitle";
+import { ProductCardHOCProps } from "../interfaces/interfaces";
 
 
 export { ProductButtons } from "./ProductButtons";
@@ -9,7 +10,7 @@ export { ProductImage } from "./ProductImage";
 export { ProductTitle } from "./ProductTitle";
 
 
-export const ProductCard = Object.assign(ProductCardHOC, {
+export const ProductCard: ProductCardHOCProps = Object.assign(ProductCardHOC, {
     Title: ProductTitle,
     Image: ProductImage,
     Buttons: ProductButtons
